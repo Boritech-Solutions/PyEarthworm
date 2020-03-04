1e5fff792a85cf6c996a7174a58477e4f5996a8a
Chris Malek <cmalek@caltech.edu>
2020-03-03 16:43:29 -0800

    Got the correct version

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='PyEarthworm',
-    version='1.0.0',
+    version='1.3',
     author='Francisco J Hernandez Ramirez',
     url='https://github.com/Boritech-Solutions/PyEarthworm',
     ext_modules=cythonize(Extension('PyEW', ['src/PyEW.pyx'])),
